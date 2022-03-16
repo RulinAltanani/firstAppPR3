af40412e1a363f7abb33cea0604051d41a83b8c6
Rulin Altanani <61116117+RulinAltanani@users.noreply.github.com>
2022-03-16 15:15:44 +0200

    Update MainTest.java

--- src/Assiement1Introduction/MainTest.java
+++ src/Assiement1Introduction/MainTest.java
@@ -17,41 +17,7 @@ import java.io.PrintWriter;
  */
 public class MainTest {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) {
+     System.out.print("Hi there");  }   
 
-        Student students[]
-                = {
-                    new ItStudent(12, "Rulin", "cs", 20,40, 10),
-                    new ItStudent(3, "Rulin ", "se", 27, 58, 9),
-                    new ItStudent(4, "Rulin", "cs", 28, 51, 8),
-                    new ArtStudent(2, "Mohammed", "b", 30, 51, 8),
-                    new ArtStudent(2, "RoRO", "b", 25, 51, 10),
-                    new ArtStudent(2, "MeMe", "b", 21, 55, 8)
-
-                };
-
-        for (int i = 0; i < students.length; i++) {
-            for (int j = 0; j < students.length - 1; j++) {
-                if (students[j].getGrade() > students[j + 1].getGrade()) {
-                    Student temp = students[j + 1];
-                    students[j + 1] = students[j];
-                    students[j] = temp;
-                }
-
-            }
-
-        }
-
-        File f = new File("file1.txt");
-        if(!f.exists()){
-        f.createNewFile();}
-        PrintWriter outOfStrem;
-        outOfStrem = new PrintWriter(new FileOutputStream(f, true));
-        for (Student student : students) {
-            outOfStrem.println(student);
-        }
-
-        outOfStrem.close();
-
-    }
 }
